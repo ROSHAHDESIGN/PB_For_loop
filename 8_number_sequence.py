63ce7b02e8e1b6f4a963e38a5c65955071aa4e49
Rozalina Shahanska <129857786+ROSHAHDESIGN@users.noreply.github.com>
2024-11-19 19:15:06 +0200

    Update 8_number_sequence.py

--- 8_number_sequence.py
+++ 8_number_sequence.py
@@ -1,1 +1,14 @@
- 
+import sys
+
+smallest = sys.maxsize
+biggest = - sys.maxsize
+
+n = int(input())
+for i in range(0,n):
+    num = int(input())
+    if num < smallest:
+        smallest = num
+    elif num > biggest:
+        biggest = num
+print(smallest)
+print(biggest)
