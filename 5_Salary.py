c1901b0e95c0a888d4f9926c29dc0a1f385cb452
Rozalina Shahanska <129857786+ROSHAHDESIGN@users.noreply.github.com>
2024-11-20 11:25:00 +0200

    Update 5_Salary.py

--- 5_Salary.py
+++ 5_Salary.py
@@ -2,9 +2,22 @@
 # •	"Instagram" -> 100 лв.
 # •	"Reddit" -> 50 лв.
 
-tabs = int(input())
-salary = int(input())
+opened_tabs = int(input())  # 4
+salary = float(input())
 
-for tab in range(tabs): #for tab in range(4):[0,1,2,3]
-    current_tab = input()
+for n in range(opened_tabs):
+    current_web_site = input()
+    if current_web_site == "Facebook":
+        salary -= 150
+    elif current_web_site == "Instagram":
+        salary -= 100
+    elif current_web_site == "Reddit":
+        salary -= 50
 
+    if salary <= 0:
+        break
+
+if salary <= 0:
+    print("You have lost your salary.")
+else:
+    print(int(salary))
